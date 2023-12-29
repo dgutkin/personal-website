7237b7e1fbef0f0a471c1c13175694370cdccf8e
Daniel Gutkin <dngutkin@gmail.com>
2023-12-28 22:31:16 -0500

    Styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import '../styles/NavBar.css';
+
 function NavBar() {
+
+    const aboutClick = (event) => {
+        // scroll to the About page
+    }
+
+    const projectClick = (event) => {
+        // scroll to the Projects page
+    }
+
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#about">Dan Gutkin</Navbar.Brand>
+            <Container fluid className="navbar-container">
+                <Navbar.Brand href="#"></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav className="me-auto" variant="underline">
+                        <Nav.Link href="#" onClick={aboutClick}>About</Nav.Link>
+                        <Nav.Link href="#projects" onCLick={projectClick}>Projects</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
