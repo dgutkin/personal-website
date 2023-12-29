db7954c60daf17fc8256e2a57cd1da1c2baab034
Daniel Gutkin <dngutkin@gmail.com>
2023-12-28 20:24:28 -0500

    Defined project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './styles/App.css';
+
+import NavBar from './components/NavBar';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <About/>
+      <Projects/>
     </div>
   );
 }
