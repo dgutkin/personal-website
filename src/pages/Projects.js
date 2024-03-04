67d6bff54a430c49952f31d15a377ba61ffb3013
Daniel Gutkin <dngutkin@gmail.com>
2024-03-04 15:13:56 -0500

    Reshuffle projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -18,11 +17,42 @@ function Projects() {
             <Row>
                 <p id="name">Projects</p>
                 <br/> 
-                <p>Some of my work...</p>
+                <p>Some adventures...</p>
             </Row>
 
             <Row>
                 <Col className="card-list">
+
+                    <Card className="project-card">
+                        <Card.Img className="card-image" src="Mac.jpeg" />
+                        <Card.Body>
+                            <Card.Title>Stealth Project</Card.Title>
+                            <Card.Text>
+                                Under construction.
+                            </Card.Text>
+                            <div className="link-container">
+                            </div>
+                        </Card.Body>
+                    </Card>
+
+                    <Card className="project-card">
+                        <Card.Img className="card-image" src="Code.jpeg" />
+                        <Card.Body>
+                            <Card.Title>runPen</Card.Title>
+                            <Card.Text>
+                                Digital running journal. Built with Next.js, Express and MongoDB.
+                            </Card.Text>
+                            <div className="link-container">
+                                <a className="link-button" href="http://runpen.ca">
+                                    <FontAwesomeIcon icon={faPlay} size="2xl"/>
+                                </a>
+                                <a className="link-button" href="https://github.com/dgutkin/running-journal">
+                                    <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
+                                </a>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                
 
                     <Card className="project-card">
                         <Card.Img className="card-image" src="Phone.jpeg" />
@@ -32,7 +62,7 @@ function Projects() {
                                 Rubber ducky themed clicker game for iOS. Built in Unity.
                             </Card.Text>
                             <div className="link-container">
-                            <a className="link-button" href="https://www.apple.com/ca/search/quacky-clicker?src=globalnav">
+                                <a className="link-button" href="https://www.apple.com/ca/search/quacky-clicker?src=globalnav">
                                     <FontAwesomeIcon icon={faPlay} size="2xl"/>
                                 </a>
                                 <a className="link-button" href="https://github.com/dgutkin/Quacky-Clicker">
@@ -41,38 +71,7 @@ function Projects() {
                             </div>
                         </Card.Body>
                     </Card>
-
-                    <Card className="project-card">
-                        <Card.Img className="card-image" src="Mac.jpeg" />
-                        <Card.Body>
-                            <Card.Title>Dad Jokes</Card.Title>
-                            <Card.Text>
-                                Dad joke generator on the web. Built with React.js.
-                            </Card.Text>
-                            <div className="link-container">
-                                <a className="link-button" href="https://github.com/dgutkin/dad-jokes">
-                                    <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
-                                </a>
-                            </div>
-                        </Card.Body>
-                    </Card>
-
-                    <Card className="project-card">
-                        <Card.Img className="card-image" src="Code.jpeg" />
-                        <Card.Body>
-                            <Card.Title>Running Journal</Card.Title>
-                            <Card.Text>
-                                Run journalling on the web. Built with Next.js and Express.
-                            </Card.Text>
-                            <div className="link-container">
-                                <a className="link-button" href="https://github.com/dgutkin/running-journal">
-                                    <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
-                                </a>
-                            </div>
-                        </Card.Body>
-                    </Card>
                 </Col>
-                
             </Row>
         </Container>
     );
