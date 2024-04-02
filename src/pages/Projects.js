563c0c236c1988b8a4c8b40151594323433ea462
Daniel Gutkin <dngutkin@gmail.com>
2024-04-02 16:52:49 -0400

    Add project icons

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,6 +6,9 @@ import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+import { faGamepad } from '@fortawesome/free-solid-svg-icons'
 
 import '../styles/App.css';
 import '../styles/Projects.css';
@@ -30,18 +33,25 @@ function Projects() {
                     <Card className="project-card">
                         
                         <Card.Body>
-                            <Card.Title>DineSafeTO Infraction Rates</Card.Title>
-                            <Card.Text>
-                                Restaurant clustering and inspection infraction rates.
-                            </Card.Text>
-                            <div className="link-container">
-                                <a className="link-button" href="https://nbviewer.org/github/dgutkin/dinesafe-infraction-rates/blob/main/dinesafe-infractions.ipynb">
-                                    <FontAwesomeIcon icon={faPlay} size="2xl"/>
-                                </a>
-                                <a className="link-button" href="https://github.com/dgutkin/dinesafe-infraction-rates">
-                                    <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
-                                </a>
-                            </div>
+                            <Row>
+                            <Col>
+                                <Card.Title>DineSafeTO Infractions</Card.Title>
+                                <Card.Text>
+                                    Restaurant clustering and inspection infraction rates.
+                                </Card.Text>
+                                <div className="link-container">
+                                    <a className="link-button" href="https://nbviewer.org/github/dgutkin/dinesafe-infraction-rates/blob/main/dinesafe-infractions.ipynb">
+                                        <FontAwesomeIcon icon={faPlay} size="2xl"/>
+                                    </a>
+                                    <a className="link-button" href="https://github.com/dgutkin/dinesafe-infraction-rates">
+                                        <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
+                                    </a>
+                                </div>
+                            </Col>
+                            <Col xs={3} className="project-logo">
+                                <FontAwesomeIcon icon={faUtensils} style={{color: "#c2c2c2"}}/>
+                            </Col>
+                            </Row>
                         </Card.Body>
                     </Card>
 
@@ -58,37 +68,52 @@ function Projects() {
                     <Card className="project-card">
                         
                         <Card.Body>
-                            <Card.Title>runPen</Card.Title>
-                            <Card.Text>
-                                Digital running journal for the web.
-                            </Card.Text>
-                            <div className="link-container">
-                                <a className="link-button" href="https://runpen.ca">
-                                    <FontAwesomeIcon icon={faPlay} size="2xl"/>
-                                </a>
-                                <a className="link-button" href="https://github.com/dgutkin/runpen-client">
-                                    <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
-                                </a>
-                            </div>
+                            <Row>
+                            <Col>
+                                <Card.Title>runPen</Card.Title>
+                                <Card.Text>
+                                    Digital running journal for the web.
+                                </Card.Text>
+                                <div className="link-container">
+                                    <a className="link-button" href="https://runpen.ca">
+                                        <FontAwesomeIcon icon={faPlay} size="2xl"/>
+                                    </a>
+                                    <a className="link-button" href="https://github.com/dgutkin/runpen-client">
+                                        <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
+                                    </a>
+                                </div>
+                            </Col>
+                            <Col xs={3} className="project-logo">
+                                <FontAwesomeIcon icon={faBook} style={{color: "#c2c2c2"}}/>
+                            </Col>
+                            </Row>
                         </Card.Body>
+                        
                     </Card>
                 
 
                     <Card className="project-card project-card-non-first">
                         
                         <Card.Body>
-                            <Card.Title>Quacky Clicker</Card.Title>
-                            <Card.Text>
-                                Rubber duck clicker game for iOS. 100+ players.
-                            </Card.Text>
-                            <div className="link-container">
-                                <a className="link-button" href="https://apps.apple.com/us/app/quacky-clicker/id1512702178">
-                                    <FontAwesomeIcon icon={faPlay} size="2xl"/>
-                                </a>
-                                <a className="link-button" href="https://github.com/dgutkin/Quacky-Clicker">
-                                    <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
-                                </a>
-                            </div>
+                            <Row>
+                            <Col>
+                                <Card.Title>Quacky Clicker</Card.Title>
+                                <Card.Text>
+                                    Rubber duck clicker game for iOS. 100+ players.
+                                </Card.Text>
+                                <div className="link-container">
+                                    <a className="link-button" href="https://apps.apple.com/us/app/quacky-clicker/id1512702178">
+                                        <FontAwesomeIcon icon={faPlay} size="2xl"/>
+                                    </a>
+                                    <a className="link-button" href="https://github.com/dgutkin/Quacky-Clicker">
+                                        <FontAwesomeIcon icon={faSquareGithub} size="2xl"/>
+                                    </a>
+                                </div>
+                            </Col>
+                            <Col xs={3} className="project-logo">
+                                <FontAwesomeIcon icon={faGamepad} style={{color: "#c2c2c2"}}/>
+                            </Col>
+                            </Row>
                         </Card.Body>
                     </Card>
                 </Col>
